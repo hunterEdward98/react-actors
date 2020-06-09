0afcc19c704dd4cd52e7526e3a71283836e70c6e
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-09 11:56:57 -0500

    finished baseline

---
+++ Wk4/react-actors/src/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class About extends Component {
+    render() {
+        return (
+            <div className="About">
+                <h2>We are actors. We use other Actors, who
+                sometimes use other Actors. We react to things
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ Wk4/react-actors/src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="Contact">
+                <p>Phone: 1-800-WE-REACT</p>
+                <p>Email: info@ReactActors.com</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
